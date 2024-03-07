fac82ef0b69b103078aabb43ede37c1b2b0018c2
misha <mikhail.dyoshin@gmail.com>
2024-03-07 10:44:44 +0300

    Created the TrackListItem-composable, refactored the TrackList.
    TrackListItem is used to display single TrackState and react on user's click.

--- app/src/main/java/com/example/musicplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/musicplayerapp/MainActivity.kt
@@ -36,19 +36,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MusicPlayerAppTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/musicplayerapp/presentation/playerscreen/components/TrackList.kt
+++ app/src/main/java/com/example/musicplayerapp/presentation/playerscreen/components/TrackList.kt
@@ -3,14 +3,14 @@ package com.example.musicplayerapp.presentation.playerscreen.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.musicplayerapp.presentation.playerscreen.state.TrackState
 
 @Composable
-fun TrackList(tracks: List<TrackState>) {
+fun TrackList(tracks: List<TrackState>, onTrackClick: (track: TrackState) -> Unit) {
 
     Column(
         modifier = Modifier.fillMaxWidth(),
@@ -18,8 +18,23 @@ fun TrackList(tracks: List<TrackState>) {
         verticalArrangement = Arrangement.Center
     ) {
         tracks.forEach {
-            Text(text = it.trackName)
+            TrackListItem(track = it, onTrackClick = { onTrackClick(it) })
         }
     }
 
+}
+
+@Preview
+@Composable
+fun TrackListPreview() {
+
+    val trackList = listOf(
+        TrackState(trackName = "Track 1", artistName = "Android", isSelected = false),
+        TrackState(trackName = "Track 2", artistName = "Android", isSelected = false),
+        TrackState(trackName = "Track 3", artistName = "Android", isSelected = true),
+        TrackState(trackName = "Track 4", artistName = "Android", isSelected = false),
+        TrackState(trackName = "Track 5", artistName = "Android", isSelected = false),
+    )
+
+    TrackList(tracks = trackList, onTrackClick = {})
 }
