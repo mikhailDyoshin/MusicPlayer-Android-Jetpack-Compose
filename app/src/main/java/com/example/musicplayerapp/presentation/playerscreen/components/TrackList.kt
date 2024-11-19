55c6acd3bda66bbec9829f51ab610c02e7121737
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2024-11-19 21:34:02 +0300

    Refactored the TrackList- and TrackListItem-composables

--- app/src/main/java/com/example/musicplayerapp/presentation/playerscreen/components/TrackList.kt
+++ app/src/main/java/com/example/musicplayerapp/presentation/playerscreen/components/TrackList.kt
@@ -2,14 +2,12 @@ package com.example.musicplayerapp.presentation.playerscreen.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.musicplayerapp.presentation.playerscreen.previewData.PlayerScreenPreviewData
 import com.example.musicplayerapp.presentation.playerscreen.state.TrackUIState
 
@@ -21,8 +19,7 @@ fun TrackList(
 ) {
     LazyColumn(
         modifier = modifier
-            .fillMaxSize()
-            .padding(top = 10.dp),
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
--- app/src/main/java/com/example/musicplayerapp/presentation/playerscreen/components/TrackListItem.kt
+++ app/src/main/java/com/example/musicplayerapp/presentation/playerscreen/components/TrackListItem.kt
@@ -6,12 +6,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,8 +35,6 @@ fun TrackListItem(track: TrackUIState, onTrackClick: () -> Unit) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
-            .padding(vertical = 5.dp, horizontal = 5.dp)
-            .clip(shape = RoundedCornerShape(15.dp))
             .background(color = bgColor)
             .clickable(onClick = { onTrackClick() })
     ) {
