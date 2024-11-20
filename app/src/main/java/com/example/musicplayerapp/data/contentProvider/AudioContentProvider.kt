da7d4699f9acc6f2b0bfba62c901147b9a06802f
Mikhail Dyoshin <mikhail.dyoshin@gmail.com>
2024-11-20 16:09:04 +0300

    Added 'id'-property for the AudioItemStorageModel. Refactored the AudioContentProvider

--- app/src/main/java/com/example/musicplayerapp/data/contentProvider/AudioContentProvider.kt
+++ app/src/main/java/com/example/musicplayerapp/data/contentProvider/AudioContentProvider.kt
@@ -8,43 +8,45 @@ import com.example.musicplayerapp.data.storage.models.AudioItemStorageModel
 
 class AudioContentProvider(private val app: Context) {
 
+    private val idProjection = MediaStore.MediaColumns._ID
     private val displayNameProjection = MediaStore.Audio.AudioColumns.DISPLAY_NAME
 
     private val projection = arrayOf(
+        idProjection,
         displayNameProjection
     )
 
     private val selection = null
-
     private val selectionArgs = null
-
     private val sortOrder = null
 
     fun getContent(uri: Uri): AudioItemStorageModel? {
-
-        val fileName = app.contentResolver.query(
+        return app.contentResolver.query(
             uri,
             projection,
             selection,
             selectionArgs,
             sortOrder
         )?.use { cursor ->
+            val idIndex = cursor.getColumnIndex(idProjection)
+            val nameIndex = cursor.getColumnIndex(displayNameProjection)
 
-            val index = cursor.getColumnIndex(displayNameProjection)
-            cursor.moveToFirst()
-            cursor.getString(index)
+            if (cursor.moveToFirst()) {
+                val id = cursor.getLong(idIndex)
+                val fullFileName = cursor.getString(nameIndex)
 
-        }
-        if (fileName == null) {
-            Log.d("Audio names", "Query is null")
-        }
-        return fileName?.let { fullFileName ->
-            val name = Uri.parse(fullFileName).lastPathSegment ?: "No name"
-            Log.d(TAG, "$name -> $fullFileName\n$uri\n")
-            AudioItemStorageModel(
-                name = Uri.parse(fullFileName).lastPathSegment ?: "No name",
-                uri = uri
-            )
+                val name = Uri.parse(fullFileName).lastPathSegment ?: "No name"
+                Log.d(TAG, "$name -> $fullFileName\n$uri\n")
+
+                AudioItemStorageModel(
+                    id = id,
+                    name = name,
+                    uri = uri
+                )
+            } else {
+                Log.d(TAG, "Cursor is empty for URI: $uri")
+                null
+            }
         }
     }
 
